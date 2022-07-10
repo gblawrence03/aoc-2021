e68b03e254dfd710d23ffa197456b9eee2ee7af1
GEORGE LAWRENCE <georgeblawrence@hotmail.co.uk>
2022-07-10 12:54:43 +0100

    Day 3 complete

--- src/day3.py
+++ src/day3.py
@@ -3,11 +3,12 @@ import os
 file = open(os.path.join(os.path.dirname(__file__), "../data/day3.txt"), 'r')
 lines = file.read().splitlines()
 num_lines = len(lines)
+bits_per_line = len(lines[0])
+
+# PART 1
 
 gamma = 0
 epsilon = 0
-
-bits_per_line = len(lines[0])
 
 for i in range(bits_per_line):
     ones = sum([int(lines[j][i]) for j in range(num_lines)])
@@ -17,3 +18,39 @@ for i in range(bits_per_line):
         epsilon += 2**(bits_per_line - i - 1)
 
 print(epsilon * gamma)
+
+# PART 2
+
+remaining = lines
+cur_bit = 0
+
+while len(remaining) > 1:
+    num_remaining = len(remaining)
+    ones = sum([int(remaining[j][cur_bit]) for j in range(num_remaining)])
+    if ones >= num_remaining / 2:
+        remaining = list(filter(lambda line: line[cur_bit] == '1', remaining))
+    else:
+        remaining = list(filter(lambda line: line[cur_bit] == '0', remaining))
+    cur_bit += 1
+
+ox = remaining[0]
+
+remaining = lines
+cur_bit = 0
+
+while len(remaining) > 1:
+    num_remaining = len(remaining)
+    ones = sum([int(remaining[j][cur_bit]) for j in range(num_remaining)])
+    if ones >= num_remaining / 2:
+        remaining = list(filter(lambda line: line[cur_bit] == '0', remaining))
+    else:
+        remaining = list(filter(lambda line: line[cur_bit] == '1', remaining))
+    cur_bit += 1
+
+scrub = remaining[0]
+
+print(int(ox, 2) * int(scrub, 2))
+
+
+
+
